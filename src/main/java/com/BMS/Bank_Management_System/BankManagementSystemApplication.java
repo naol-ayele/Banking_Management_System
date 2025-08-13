0889ec4aa95916ca9217674bc389307b0e00acc8
Naol Ayele <nainak20002@gmail.com>
2025-08-13 16:32:35 -0700

    feat: setuping structure

---
+++ src/main/java/com/BMS/Bank_Management_System/BankManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.BMS.Bank_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagementSystemApplication.class, args);
+	}
+
+}
