11b15e132d1b5f35a539cbdf4b401b90fe29539a
Negasa <negareta83@gmail.com>
2025-08-17 08:07:38 +0300

    added the Role.java and TransactionRepository.java

---
+++ src/main/java/com/BMS/Bank_Management_System/entity/Role.java
@@ -0,0 +1,7 @@
+package com.BMS.Bank_Management_System.entity;
+
+public enum Role {
+    ADMIN,
+    STAFF,
+    CUSTOMER
+}
---
+++ src/main/java/com/BMS/Bank_Management_System/repository/TransactionRepository.java
@@ -0,0 +1,15 @@
+package com.BMS.Bank_Management_System.repository;
+
+import com.BMS.Bank_Management_System.entity.Role;
+import com.BMS.Bank_Management_System.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List <Transaction> findByAccount_Id(Long accountId);
+    List <Transaction> findByPerformedBy_Username(String username);
+    List <Transaction> findByPerformedBy_Role(Role role);
+    List <Transaction> findByAccount_AccountNumber(String accountNumber);
+
+}
