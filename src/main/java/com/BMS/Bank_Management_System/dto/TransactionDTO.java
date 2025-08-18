2a49fa12c7875d10c3eb74ecbea74b6b6a5990c2
Negasa <negareta83@gmail.com>
2025-08-18 23:38:06 +0300

    Transaction DTO

---
+++ src/main/java/com/BMS/Bank_Management_System/dto/TransactionDTO.java
@@ -0,0 +1,22 @@
+package com.bms.Banking.Management.System.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionDTO {
+    private Long id;
+    private String type; // DEPOSIT, WITHDRAW, TRANSFER
+    private BigDecimal amount;
+    private LocalDateTime timestamp;
+    private String description;
+    private Long accountId;
+    private String performedByUsername;
+
+}
