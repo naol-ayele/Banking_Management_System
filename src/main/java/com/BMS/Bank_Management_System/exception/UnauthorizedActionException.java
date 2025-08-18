2e3ee85541a01a8c817c43645cd09319ae7b6015
giftiy <giftbulti59@gmail.com>
2025-08-18 12:08:22 +0300

    Updated UnauthorizedActionException

--- src/main/java/com/BMS/Bank_Management_System/exception/UnauthorizedActionException.java
+++ src/main/java/com/BMS/Bank_Management_System/exception/UnauthorizedActionException.java
@@ -1,4 +1,8 @@
 package com.BMS.Bank_Management_System.exception;
 
-public class UnauthorizedActionException {
+
+public class UnauthorizedActionException extends RuntimeException {
+    public UnauthorizedActionException(String message) {
+        super(message);
+    }
 }
