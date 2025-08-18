50473f9e9f072884d6294aa7c7d233b97141826e
giftiy <giftbulti59@gmail.com>
2025-08-18 12:08:22 +0300

    Updated JwtUtil helper class

--- src/main/java/com/BMS/Bank_Management_System/util/JwtUtil.java
+++ src/main/java/com/BMS/Bank_Management_System/util/JwtUtil.java
@@ -1,4 +1,60 @@
 package com.BMS.Bank_Management_System.util;
 
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
 public class JwtUtil {
+
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt.expiration}")
+    private Long jwtExpirationMs;
+
+
+    // Generate token
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .compact();
+    }
+
+    // Validate token
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+
+    // Extract username from token
+    public String getUsernameFromJwtToken(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+
+
+
 }
