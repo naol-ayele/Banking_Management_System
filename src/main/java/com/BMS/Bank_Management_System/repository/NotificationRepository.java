4f1b10dd3d9cbd0e9a68c1dc4d7ca2a0d9e8a13c
Bontu Negasa <bontu30345@gmail.com>
2025-08-18 06:37:03 -0700

    feat:added notification service.

--- src/main/java/com/BMS/Bank_Management_System/repository/NotificationRepository.java
+++ src/main/java/com/BMS/Bank_Management_System/repository/NotificationRepository.java
@@ -1,4 +1,14 @@
 package com.BMS.Bank_Management_System.repository;
 
-public interface NotificationRepository {
+
+
+import com.BMS.Bank_Management_System.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUser_UsernameOrderByCreatedAtDesc(String username);
 }
+
+
