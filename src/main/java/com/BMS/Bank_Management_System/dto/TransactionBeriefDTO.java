ccfb4864654d15e7e171b48f58e776320bee3b38
Negasa <negareta83@gmail.com>
2025-08-18 22:59:22 +0300

    Transaction berief DTO

---
+++ src/main/java/com/BMS/Bank_Management_System/dto/TransactionBeriefDTO.java
@@ -0,0 +1,24 @@
+package com.bms.Banking.Management.System.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionBriefDTO {
+    private Long id;
+    private String type;
+    private BigDecimal amount;
+    private LocalDateTime timestamp;
+    private String description;
+    private String referenceId;
+    private String status;
+    private String performedBy; // username
+}
+
+
