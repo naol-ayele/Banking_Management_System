01a6cf6f90869a419dfed51f6b6e9003e1d4f21d
abdic demissu <abdibig641@gmail.com>
2025-08-19 10:27:17 +0300

    refactor(dto): update AccountDTO with new fields and validations

--- src/main/java/com/BMS/Bank_Management_System/dto/AccountDTO.java
+++ src/main/java/com/BMS/Bank_Management_System/dto/AccountDTO.java
@@ -1,4 +1,19 @@
 package com.BMS.Bank_Management_System.dto;
 
+    import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountDTO {
+    private Long id;
+    private String accountNumber;
+    private BigDecimal balance;
+    private Long userId;
+    private String accountType; // SAVINGS, CURRENT
+    private String status; // PENDING_APPROVAL, ACTIVE, FROZEN, CLOSED
 }
