ca58b1d68cf288ad2343dca2c10d70096fa81bef
abdic demissu <abdibig641@gmail.com>
2025-08-19 10:31:53 +0300

    feat(repository): extend AccountRepository with custom query methods

--- src/main/java/com/BMS/Bank_Management_System/repository/AccountRepository.java
+++ src/main/java/com/BMS/Bank_Management_System/repository/AccountRepository.java
@@ -1,4 +1,12 @@
 package com.BMS.Bank_Management_System.repository;
 
-public class AccountRepository {
+import com.BMS.Bank_Management_System.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByAccountNumber(String accountNumber);
+    List<Account> findByUser_Username(String username);
 }
