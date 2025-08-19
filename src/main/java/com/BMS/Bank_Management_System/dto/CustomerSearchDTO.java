26a3d93a948e08ab72a3a161398be8ee836aa573
Naol Ayele <nainak20002@gmail.com>
2025-08-19 05:17:42 -0700

    feat: adding customer search dto

--- src/main/java/com/BMS/Bank_Management_System/dto/CustomerSearchDTO.java
+++ src/main/java/com/BMS/Bank_Management_System/dto/CustomerSearchDTO.java
@@ -1,4 +1,17 @@
 package com.BMS.Bank_Management_System.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerSearchDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String phone;
+    private int accountCount;
+    private String status; // ACTIVE, LOCKED, etc.
 }
