267544fa47cda7da8f714fd004cadefe22481abb
Negasa <negareta83@gmail.com>
2025-08-19 15:57:05 +0300

    transaction berief dto

--- src/main/java/com/BMS/Bank_Management_System/dto/TransactionBeriefDTO.java
+++ src/main/java/com/BMS/Bank_Management_System/dto/TransactionBeriefDTO.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class TransactionBriefDTO {
+public class TransactionBeriefDTO {
     private Long id;
     private String type;
     private BigDecimal amount;
