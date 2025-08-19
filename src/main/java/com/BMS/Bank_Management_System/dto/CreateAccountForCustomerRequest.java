7d352795db5bddaad17462b0f2da509b4cc6f300
abdic demissu <abdibig641@gmail.com>
2025-08-19 10:28:32 +0300

    feat(dto): add request DTO for creating account for existing customer

--- src/main/java/com/BMS/Bank_Management_System/dto/CreateAccountForCustomerRequest.java
+++ src/main/java/com/BMS/Bank_Management_System/dto/CreateAccountForCustomerRequest.java
@@ -1,4 +1,17 @@
 package com.BMS.Bank_Management_System.dto;
 
+ import lombok.AllArgsConstructor;
+ import lombok.Data;
+ import lombok.NoArgsConstructor;
+
+ import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateAccountForCustomerRequest {
+    private String accountType;
+    private BigDecimal openingBalance;
 }
+
+
